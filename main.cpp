67df62493995115f4cf04084dad97485a3716539
Shamik Patro <patroshamik@gmail.com>
2022-12-08 15:26:02 -0600

    Changes to Main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
 #include "Media.h"
 #include "Movies.h"
@@ -7,24 +8,107 @@
 #include "Podcasts.h"
 #include "TV.h"
 
-int main(){
 
+void sorting(){
+
+    
+
+
+
+
+
+}
+
+
+
+void storing(){
+
+    system("clear");
     std::string choice;
+    std::cout << "\n------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "Welcome to Storing! \n\nDBManager has detected the following devices: \t\t\t|\n" << std::endl;
+    std::cout << "> Toshiba Gigabeat MP3 Player" << std::endl;
+    std::cout << "> Galaxy S3 Smartphone Device" << std::endl;
+    std::cout << "> Nikon Rebel T4 DSLR Camera\n" << std::endl;
+    std::cout << "Please choose a device: " << std::endl;
+    std::cout << "------------------------------------------------------------------------------------------\n" << std::endl;
+    std::cout << "Devices: \t\t\t\t Explanation:" << std::endl;
     std::cout << std::endl;
-    std::cout << "Welcome to the database! \nPlease choose a letter from the following list: " << std::endl;
-    std::cout << "-----------------------------------------------\n" << std::endl;
-    std::cout << "A: Print all content in the database" << std::endl;
-    std::cout << "B: Print only music, only movies, only TV shows, only podcasts, or only videos" <<  std::endl;
-    std::cout << "C: Print only content that can be listened to" << std::endl;
-    std::cout << "D: Print only content that can be viewed" << std::endl;
-    std::cout << "E: Quit the program" << std::endl;
+    std::cout << "A: Toshiba Gigabeat MP3 Player" << std::endl;
+    std::cout << "B: Galaxy S3 Smartphone Device" << std::endl;
+    std::cout << "C: Rebel T4 DSLR Camera" << std::endl;
+    std::cout << "D: Exit Program" << std::endl;
     std::cout << std::endl;
     std::cout << "Choice: ";
     std::getline(std::cin, choice);
 
-    // Input verification
+}
 
-    if (choice != "A" || choice != "a" || choice != "B" || choice != "b")
+
+void printing(){
+
+    system("clear");
+    std::string choice;
+    std::cout << "\n-----------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "|\t\t\t\tWelcome to Printing!\t\t\t\t\t|" << std::endl;
+    std::cout << "|\t\t\t\t\t\t\t\t\t\t\t|" << std::endl;
+    std::cout << "|\t\t\tPlease input an option to get started.\t\t\t\t|" << std::endl;
+    std::cout << "-----------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "A: Print all content in the database" << std::endl;
+    std::cout << "B: Print only music, only movies, only TV shows, only podcasts, or only videos" << std::endl;
+    std::cout << "C: Print only content that can be listened to" << std::endl;
+    std::cout << "D: Print only content that can be viewed" << std::endl;
+    std::cout << "E: Exit Program" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Choice: ";
+    std::getline(std::cin, choice);
+
+    if ( choice == "E" or choice == "e"){
+
+        
+
+    }
+
+}
+
+void mainMenu(){
+
+    system("clear");
+    std::string choice;
+    std::cout << "\n-----------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "|\t\t\tWelcome to the Main Menu of DBManager!\t\t\t\t|" << std::endl;
+    std::cout << "|\t\t\t\t\t\t\t\t\t\t\t|" << std::endl;
+    std::cout << "|\t\t\tPlease input an option to get started.\t\t\t\t|" << std::endl;
+    std::cout << "-----------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "Options: \t\t\t\t Explanation:" << std::endl;
+    std::cout << std::endl;
+    std::cout << "A. Database Printing Service\t\t Printing content from database." << std::endl; // Printing elements from Database
+    std::cout << "B. Multimedia Storing Service\t\t Storing elements from database to external device." << std::endl; // Storing elements from Database to Multimedia Device
+    std::cout << "C. Database Sorting Service\t\t Sorting database based on custom criteria." << std::endl; // Sorting elements from Database
+    std::cout << "D. Database Content Management\t\t Adding content to database." << std::endl; // Managing content within database
+    std::cout << "E. Quit the program\t\t\t Leave the program." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Choice: ";
+    std::getline(std::cin, choice);
+
+    if (choice == "A" || choice == "a"){
+
+        printing();
+
+    }
+
+    if (choice == "B" || choice == "b"){
+
+        storing();
+
+    }
+
+}
+
+
+int main(){
+
+    mainMenu();
 
     return 0;
 }
